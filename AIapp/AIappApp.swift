79f9b3fd269dad23293a48d2e0bc0f7b6c2aad2e
MurodjonT <murodjon.mmm.99@mail.ru>
2025-06-11 13:37:20 +0500

    Initial Commit

---
+++ AIapp/AIappApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIappApp.swift
+//  AIapp
+//
+//  Created by Murodjon Turobov on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AIappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
