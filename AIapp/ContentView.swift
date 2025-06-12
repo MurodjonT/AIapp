d458ac2fa4c47a24686744ec753860c4941a036b
MurodjonT <murodjon.mmm.99@mail.ru>
2025-06-12 13:58:38 +0500

    update animation gradient

--- AIapp/ContentView.swift
+++ AIapp/ContentView.swift
@@ -13,85 +13,113 @@ struct ContentView: View {
     @State private var email = ""
     @State private var password = ""
     @State private var isSecured = true
+    @State private var angle: Angle = .zero
+    
+    
     var body: some View {
         
-        ScrollView {
-            VStack {
-                Image("CPU 3")
-                    .frame(width: 75, height: 75)
-                    .padding(.top, 55)
-                Text("  IMAGINE \nWITH AIKO")
-                    .font(.system(size: 50))
-                
-                
-                CustomTextField(
-                    title: "Email address",
-                    text: $email,
-                    keyboardType: .emailAddress
+        ZStack {
+            RoundedRectangle(cornerRadius: 60)
+                .strokeBorder(
+                    AngularGradient(
+                        colors: [.red, .orange, .yellow, .green, .blue, .purple],
+                        center: .center,
+                        angle: angle
+                    ),
+                    
+                    lineWidth: 7
+                    
                 )
-                
-                CustomTextField(
-                    title: "Password",
-                    text: $password,
-                    keyboardType: .default
-                )
-                .padding(.top, 20)
-                
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        
-                    }) {
-                        Text("Forgot Password?")
-                            .font(.footnote)
-                            .foregroundColor(.black)
+                .blur(radius: 5)
+                .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 5)
+                .padding(0)
+                .onAppear {
+                    withAnimation(.linear(duration: 8).repeatForever(autoreverses: false)) {
+                        angle = .degrees(360)
                     }
                 }
-                .padding(.top, 10)
-                
-                MainButton("Create") {
-                }
-                .disabled(email.isEmpty || password.isEmpty)
-                .padding(.top)
-                
-                Divider()
-                    .padding([.top, .bottom], 20)
-                
-                
-                SignUpButton("Sign up with Google", "Google Logo") {
+            ScrollView {
+                VStack {
+                    Image("CPU 3")
+                        .frame(width: 75, height: 75)
+                        .padding(.top, 55)
+                    Text("  IMAGINE \nWITH AIKO")
+                        .font(.system(size: 50))
                     
-                }
-                
-                SignUpButton("Sign up with Apple", "Apple  Logo") {
                     
-                }
-                .padding(.top, 20)
-                
-                HStack(spacing: 40) {
-                    Button(action: {
+                    CustomTextField(
+                        title: "Email address",
+                        text: $email,
+                        keyboardType: .emailAddress
+                    )
+                    
+                    CustomTextField(
+                        title: "Password",
+                        text: $password,
+                        keyboardType: .default
+                    )
+                    .padding(.top, 20)
+                    
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            
+                        }) {
+                            Text("Forgot Password?")
+                                .font(.footnote)
+                                .foregroundColor(.black)
+                        }
+                    }
+                    .padding(.top, 10)
+                    
+                    MainButton("Create") {
+                    }
+//                    .disabled(email.isEmpty || password.isEmpty)
+                    .padding(.top)
+                    
+                    Divider()
+                        .padding([.top, .bottom], 20)
+                    
+                    
+                    SignUpButton("Sign up with Google", "Google Logo") {
                         
-                    }) {
-                        Text("Privacy Policy")
-                            .font(.footnote)
-                            .foregroundColor(.black)
                     }
                     
-                    Button(action: {
+                    SignUpButton("Sign up with Apple", "Apple  Logo") {
                         
-                    }) {
-                        Text("Terms of Service")
-                            .font(.footnote)
-                            .foregroundColor(.black)
                     }
+                    .padding(.top, 20)
+                    
+                    HStack(spacing: 40) {
+                        Button(action: {
+                            
+                        }) {
+                            Text("Privacy Policy")
+                                .font(.footnote)
+                                .foregroundColor(.black)
+                        }
+                        
+                        Button(action: {
+                            
+                        }) {
+                            Text("Terms of Service")
+                                .font(.footnote)
+                                .foregroundColor(.black)
+                        }
+                    }
+                    .padding(.top, 20)
+                    
                 }
-                .padding(.top, 20)
-
+                .padding(.horizontal, 30)
             }
-            .padding(.horizontal, 30)
+            
         }
+        .ignoresSafeArea()
+        
+        
     }
+        
 }
-
 #Preview {
     ContentView()
 }
